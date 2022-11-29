41859206591763dfd3f4aac50d191ca7d306c80e
Sanzhanov <sanzhanov21@gmail.com>
2022-11-29 16:50:34 -0500

    added new tests for main page

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,43 @@
-describe('Main page if user is unauthorized', () => {
+import MainPage from "../pages/app/main.page";
+import LoginPage from "../pages/login.page";
+import RegistrationPage from "../pages/registration.page";
 
+describe('Main page content if user is unauthorized', function () {
 
+    beforeEach(() => {
+        MainPage.open()
+    })
 
+    it('Main sign and app description exist on the page', () => {
+        MainPage.mainSign.should('be.visible').should('have.text', 'Dispatching and accounting for service companies')
+        MainPage.description.should('be.visible').should('have.text', 'The easiest way to manage the process of receiving and transmitting an order.')
+    })
+
+    it('Tab Login exists on the page and contains a link to the Login page', () => {
+        MainPage.tabLogin.should('be.visible')
+
+        MainPage.tabLogin.click()
+        cy.url().should('include', '/v5/user/login')
+        LoginPage.mainSign.should('be.visible').should('contain.text', 'Welcome back!')
+    })
+
+    it('Tab Get Started exists on the page and contains a link to the Registration page', () => {
+        MainPage.tabGetStarted.should('be.visible')
+
+        MainPage.tabGetStarted.click()
+        cy.url().should('include', '/v5/user/register')
+        RegistrationPage.mainSign.should('be.visible').should('have.text', 'Create an account as a business owner')
+    })
+})
+
+describe('Main page content if user is authorized', function () {
+
+    beforeEach(() => {
+        cy.auth(Cypress.env('TOKEN'), Cypress.env('USER_ID'))
+    })
+
+    it('Company sales and personal sales signs exist on the page', () => {
+        MainPage.companySalesSign.should('be.visible').should('have.text', 'Company Sales')
+        MainPage.personalSalesSign.should('be.visible').should('have.text', 'Personal Sales')
+    })
 })
--- cypress/pages/app/main.page.js
+++ cypress/pages/app/main.page.js
@@ -2,13 +2,19 @@ import AppPage from "./app.page";
 
 class MainPage extends AppPage {
 
+    get mainSign() { return cy.xpath('//h1[text()]') }
+    get description() { return cy.get('.mt-4.mb-5') }
+
+    get tabLogin() { return cy.get('[data-qa="login"]') }
+    get tabGetStarted() { return cy.get('[data-qa="register"]') }
+
+
     get companySalesSign() { return cy.xpath('//h3[text() = "Company Sales"]') }
     get personalSalesSign() { return cy.xpath('//h3[text() = "Personal Sales"]') }
 
     open() {
         return super.open(`/v5`)
     }
-
 }
 
 export default new MainPage()
