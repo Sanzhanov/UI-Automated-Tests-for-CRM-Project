99eef815a074d3bb24609e34319bd8ce7b044028
Sanzhanov <sanzhanov21@gmail.com>
2022-12-06 22:51:35 -0500

    changed user profile page

--- cypress/pages/app/user-profile.page.js
+++ cypress/pages/app/user-profile.page.js
@@ -2,6 +2,12 @@ import AppPage from "./app.page";
 
 class UserProfilePage extends AppPage {
 
+    get userName() { return cy.xpath('//h1[.]') }
+    get companyName() { return cy.xpath('//td[.="Company"]/..//a') }
+    get userRole() { return cy.xpath('//td[.="Roles"]/..//td[2]') }
+    get creationDate() { return cy.xpath('//td[.="Created"]/..//td[2]') }
+    get userEmail() { return cy.xpath('//td[.="Email"]/..//td[2]')}
+
     open() {
         return super.open(`/v5/profile/${Cypress.env('USER_ID')}`)
     }
